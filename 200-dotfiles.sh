3f4f61a5a4450559cdfdee46c5126307a2c6db73
wslspec <oxijas@gmail.com>
2021-07-14 19:10:34 +0200

    create local share if needed

--- 200-dotfiles.sh
+++ 200-dotfiles.sh
@@ -8,6 +8,8 @@ if [ $ans != "y" ]; then
 	exit 0
 else
 	sudo pacman -S --needed --noconfirm git zsh
+	mkdir -p $HOME/.local/share
+	mkdir -p $HOME/.local/bin
 	cd $HOME
 	git clone --separate-git-dir=$HOME/.local/share/dotfiles git@github.com:oxijas/dotfiles.git dottmp
 	rsync --recursive --verbose --exclude '.git' dottmp/ $HOME/
