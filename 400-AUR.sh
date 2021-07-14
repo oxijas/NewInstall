5742c7dd9924e994d9661563203795c91698741c
wslspec <oxijas@gmail.com>
2021-07-14 19:03:34 +0200

    initial adding files

---
+++ 400-AUR.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+paru -S --noconfirm --needed lf-git vidir pfetch
+
+#this is for image preview in lf
+paru -S --noconfirm --needed ueberzug ffmpegthumbnailer imagemagick poppler epub-thumbnailer-git bat chafa unzip
+
