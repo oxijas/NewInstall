3824a0b5310c0b4d48473c11f3135a8673950de7
arch2spectr <oxijas@gmail.com>
2021-07-16 23:58:10 +0200

    some tweaks

--- 100-base-terminal.sh
+++ 100-base-terminal.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-pacman-key --init
-pacman-key --populate
+#pacman-key --init
+#pacman-key --populate
 sudo pacman -Syu
 sudo pacman -S --noconfirm --needed zsh nnn git enca rsync base-devel bat openssh
-sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar
+sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar man
 
-printf "git username :"
+echo "git username :"
 read usr
-echo " "
+echo "git email :"
 printf "email :"
 read email
 
 #NAME=$(hostname|tr '[:upper:]' '[:lower:]')
 git config --global user.email "$email"
-git config --global user.name "$ans"
+git config --global user.name $ans
 git config --global --list
 
 
--- 600-keygen.sh
+++ 600-keygen.sh
@@ -3,14 +3,7 @@
 ssh-keygen -t rsa -b 4096 -C "oxijas@gmail.com"
 eval "$(ssh-agent -s)"
 
-if [[ ! -f $HOME/.ssh/config ]]; then
-	echo "Host" >$HOME/.ssh/config
-	echo "  Hostname xx" >>$HOME/.ssh/config
-	echo "  User xx" >>$HOME/.ssh/config
-	echo "  Port xx" >>$HOME/.ssh/config
-	echo "  IdentityFile $HOME/.ssh/id_rsa" >>$HOME/.ssh/config
-fi
-printf "copy public key to github\n\n"
+echo "copy public key to github\n\n"
 cat $HOME/.ssh/id_rsa.pub
 
 #werkt alleen in X
