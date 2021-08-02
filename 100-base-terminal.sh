5433e5c5d771528309b2b2dc7114011e9c8f1aa0
wslspectr15 <oxijas@gmail.com>
2021-08-02 11:34:01 +0200

    added python and pip and psutil

--- 100-base-terminal.sh
+++ 100-base-terminal.sh
@@ -3,7 +3,10 @@
 #pacman-key --init
 #pacman-key --populate
 sudo pacman -Syyu
-sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar man jq bashtop gtop bspwm sxhdk zsh nnn git enca rsync base-devel bat openssh
+sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar man jq bashtop gtop bspwm sxhdk zsh nnn git enca rsync base-devel bat openssh python3 python-pip
+
+
+pip install psutil
 
 
 
