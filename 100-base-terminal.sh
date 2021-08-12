27dbe152880fa82a0ac479417803d5a964825a4d
Arch_WSL_Melis <archwslmelis@gmail.com>
2021-08-12 16:19:03 +0200

    added reflector to base

--- 100-base-terminal.sh
+++ 100-base-terminal.sh
@@ -3,7 +3,7 @@
 #pacman-key --init
 #pacman-key --populate
 sudo pacman -Syyu
-sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar man jq bashtop gtop bspwm sxhkd zsh nnn git enca rsync base-devel bat openssh python3 python-pip rofi xdotool bc
+sudo pacman -S --noconfirm --needed vifm mc neovim fzf unzip p7zip tar man jq bashtop gtop bspwm sxhkd zsh nnn git enca rsync base-devel bat openssh python3 python-pip rofi xdotool bc reflector
 
 
 pip install psutil
