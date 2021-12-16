48f3aff473c54357077b508a1bec7b2403d3b662
Arch_WSL_Melis <archwslmelis@gmail.com>
2021-12-16 04:54:29 +0100

    small updates

--- 400-AUR.sh
+++ 400-AUR.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-paru -S --noconfirm --needed lf-git vidir pfetch
+paru -S --noconfirm --needed lf-git vidir pfetch gitui-git
 
 #this is for image preview in lf
 paru -S --noconfirm --needed ueberzug ffmpegthumbnailer imagemagick poppler epub-thumbnailer-git bat chafa unzip picom-ibhagwan-git
